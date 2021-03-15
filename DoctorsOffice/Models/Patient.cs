8c81187ca8c33f30655192805825a3e81b31457a
tien96ng <tien96ng@ghmail.com>
2021-03-15 10:11:51 -0700

    Create Patient.cs with connection to Doctor.cs Model
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

---
+++ DoctorsOffice/Models/Patient.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace DoctorsOffice.Models
+{
+  public class Patient
+  {
+    public Patient()
+    {
+      this.JoinEntities = new HashSet<DoctorPatient>();
+    }
+
+    public int PatientId { get; set; }
+    public string Name { get; set; }
+    public string BirthDate { get; set; }
+    public virtual ICollection<DoctorPatient> JoinEntities { get; }
+  }
+}

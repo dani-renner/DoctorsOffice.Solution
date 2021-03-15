72f30c678235f57c4b6387114468160d89eb2992
tien96ng <tien96ng@ghmail.com>
2021-03-15 10:14:39 -0700

    Create DoctorPatientContext.cs
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

---
+++ DoctorsOffice/Models/DoctorsOfficeContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorsOffice.Models
+{
+  public class DoctorsOfficeContext : DbContext
+  {
+    public virtual DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; get; }
+    public DbSeet<DoctorPatient> DoctorPatient { get; set; }
+
+    public DoctorsOfficeContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}

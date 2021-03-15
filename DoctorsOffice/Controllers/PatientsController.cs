fe4b54201778f9ccb69a9e501d9284c56d8e95f3
tien96ng <tien96ng@gmail.com>
2021-03-15 16:06:07 -0700

    Create Index() and Create() in PatientsController w/ corresponding .cshtml
    Co-authored-by: Dani Renner <danijrenner@gmail.com>
    Co-authored-by: Woo Jin Kim <kimwoojin211@gmail.com>

---
+++ DoctorsOffice/Controllers/PatientsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using DoctorsOffice.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DoctorsOffice.Controllers
+{
+  public class PatientsController : Controller
+  {
+    private readonly DoctorsOfficeContext _db;
+
+    public PatientsController(DoctorsOfficeContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View(_db.Patients.ToList());
+    }
+    public ActionResult Create()
+    {
+      ViewBag.DoctorId = new SelectList(_db.Doctors, "DoctorId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Patient patient, int DoctorId)
+    {
+      _db.Patients.Add(patient);
+      _db.SaveChanges();
+      if (DoctorId != 0)
+      {
+        _db.DoctorPatient.Add(new DoctorPatient() { DoctorId = DoctorId, PatientId = patient.PatientId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}

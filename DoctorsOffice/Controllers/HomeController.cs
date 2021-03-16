bcdc6b87edad8d84b4bb23279e06d70f6ec9637e
Dani Renner <danijrenner@gmail.com>
2021-03-16 15:25:52 -0700

    add .vscode
    Co-authored-by:Tien Nguyen <tien96ng@gmail.com>
    Co-authored-by:Woo Jin Kim <kimwoojin211@gmail.com>

--- DoctorsOffice/Controllers/HomeController.cs
+++ DoctorsOffice/Controllers/HomeController.cs
@@ -1,14 +1,92 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using DoctorsOffice.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace ToDoList.Controllers
+// namespace DoctorsOffice.Controllers
+// {
+//   public class HomeController : Controller
+//   {
+//     private readonly DoctorsOfficeContext _db;
+
+//     public HomeController(DoctorsOfficeContext db)
+//     {
+//       _db = db;
+//     }
+
+//     [HttpGet("/")]
+//     public ActionResult Index()
+//     {
+//       return View();
+//     }
+
+//     public ActionResult Search(string name)
+//     {
+//       ViewBag.DoctorsFound = _db.Doctors.Where(doctor => doctor.Name.Contains(name));
+//       return View();
+//     }
+
+//     [HttpPost]
+//     public ActionResult Search(Doctor doctor)
+//     {
+//       var doctorsFound = _db.Doctors.Where(doc => doc.Name.Contains(doctor.Name));
+//       return RedirectToAction("Search");
+//     }
+
+//   }
+// }
+
+
+namespace DoctorsOffice.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly DoctorsOfficeContext _db;
+    private List<SelectListItem> _selectList;
+
+    public HomeController(DoctorsOfficeContext db)
+    {
+      _db = db;
+      _selectList = new List<SelectListItem>()
+      { 
+        new SelectListItem {Text = "All", Value = "All"},
+        new SelectListItem {Text = "Doctor", Value = "Doctor"},
+        new SelectListItem {Text = "Patient", Value = "Patient"}
+      };
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
       return View();
+    }
+    
+    public ActionResult Search()
+    {
+      ViewBag.Category = _selectList;
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Search(string category, string name)
+    {
+      ViewBag.Category = _selectList;
+      List<Doctor> doctorSearch = new List<Doctor>{};
+      List<Patient> patientSearch = new List<Patient> {};
+      if(category != "Patient")
+      {
+        doctorSearch = _db.Doctors.Where(Doctor => Doctor.Name.Contains(name)).ToList();
+      }
+      if(category != "Doctor")
+      {
+        patientSearch = _db.Patients.Where(Patient => Patient.Name.Contains(name)).ToList();
+      }
+      Dictionary<string,object> model = new Dictionary<string,object>(); // dynamic 
+      model.Add("DoctorResults",doctorSearch);
+      model.Add("PatientResults",patientSearch);
+      return View(model); //model gets lost between here and View. or. something. idk. 
     }
 
   }
---
+++ README.md
@@ -0,0 +1,47 @@
+# _Doctor's Office_
+
+#### _A program that manages all of the doctors and patients' data. _
+
+#### By _**Tien Nguyen, Dani Renner, Woo Jin Kim**_
+
+## _This project is not in a complete or portfolio ready state, and should not be considered representational of professional work._
+
+## Technologies Used
+
+* _C#_
+* _.NET 5.0.102_
+* _ASP.NET Core Mvc 4.8_
+* _Razor_
+* _Entity Framework Core_
+* _Git_
+
+## Description
+
+_This application allows a database to hold doctors and their patients. There is full CRUD funcionality for this many-to-many relationship._
+
+## Setup/Installation Requirements
+
+* You can clone the repository to your desktop
+* Navigate to the DoctorsOffice directory
+* Install the dotnet ef tool by entering "dotnet tool install --global dotnet-ef" in the terminal
+* Create the database by entering "dotnet ef database update" in the terminal
+* Enter "dotnet restore" in the terminal. Then "dotnet build"
+* Enter "dotnet run" in the terminal to start
+* Ctrl + click the link that populates in the terminal to view the application in the webpage. It is probably http://localhost:5000/
+* Use Ctrl + C in the terminal to quit the host and close out of the window in the browser.
+
+## Known Bugs
+
+* _None_
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
+
+## Contact Information
+
+_Woo Jin Kim (kimwoojin211@gmail.com)_
+_Dani Renner (danijrenner@gmail.com)_
+_Tien Nguyen (tien96ng@gmail.com)_
+
+Copyright (c) 2021 Woo Jin Kim, Dani Renner, and Tien Nguyen
